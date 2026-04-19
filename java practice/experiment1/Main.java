82bf6edeb6b6dc92f3ec3895ba2d5cc80bd3ca2c
Alisha <alishaofficial.xvi@gmail.com>
2026-04-19 16:43:09 +0530

    done 2 programs of hello world and printing student details with class, constructor and scanner class

---
+++ java practice/experiment1/Main.java
@@ -0,0 +1,33 @@
+package experiment1;
+import java.util.*;
+//  1.2.  Write a Java program to print the details of a student.
+class Student {
+    String name;
+    int SAP;
+
+    Student(String name, int SAP){
+        this.name = name;
+        this.SAP = SAP;
+    }
+    void PrintDetails(){
+        Student s1 = new Student(name, SAP);
+          System.out.println(s1.name);
+          System.out.println(s1.SAP);
+    }
+} 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("enter your details");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your SAP: ");
+        int s = sc.nextInt();
+        System.out.println("enter your Name: ");
+        String n = sc.nextLine();
+        Student s2 = new Student(n, s);
+
+        System.out.println(s.)
+
+    }
+    
+}
---
+++ java practice/experiment1/Test.java
@@ -0,0 +1,8 @@
+package experiment1;
+//  1.1.  Write a Java program to print “Hello World”’
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
